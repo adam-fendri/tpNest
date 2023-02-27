5b8d8e978719568c37b639ea8c39a0d0ceac76fb
AzizKlai <mohamedaziz.klai@insat.u-carthage.tn>
2023-02-27 16:39:18 +0100

    errormsg

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -6,16 +6,18 @@ import {
 } from '@nestjs/common';
 import { TodoModel } from './todo.model';
 import { TodoStatusEnum } from './todo.statusenum';
-import { TodoAddTDO } from './todoAddDTO';
-import { TodoUpdateDTO } from './todoUpdateDTO';
+import { TodoAddTDO } from './dto/todoAddDTO';  
+import { TodoUpdateDTO } from './dto/todoUpdateDTO';
 
 @Injectable()
 export class TodoService {
   private todos = [];
+  
   constructor(@Inject('randomID') private readonly randomID) {}
   getTodos() {
     return this.todos;
   }
+  
 
   addTodo(todo: TodoAddTDO) {
     if (todo.description == undefined || todo.name == undefined) {
@@ -23,7 +25,7 @@ export class TodoService {
     }
 
     this.todos.push(
-      new TodoModel(this.randomID, todo.name, todo.description, 'waiting'),
+      new TodoModel(this.randomID(), todo.name, todo.description, 'waiting'),
     );
   }
 
